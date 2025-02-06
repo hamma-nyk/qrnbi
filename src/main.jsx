dab152aa89be94b80f4bdb8fe93f4da044e9f002
hamma-nyk <yikonyk@gmail.com>
2025-02-06 13:56:47 +0700

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import './css/style.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import CreateQR from './pages/CreateQR'
+
+const router = createBrowserRouter([
+  {
+    path: "/create",
+    element: <CreateQR />,
+  },
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ src/pages/CreateQR.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+
+const CreateQR = () => {
+    return (
+        <Fragment>
+            <h1 className="text-3xl font-bold underline">Create QR</h1>
+        </Fragment>
+    )
+}
+
+export default CreateQR
